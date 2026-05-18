c3c5f5cfe7af2b1aa122d2d8faad679b51f808da
Fengchuan-Song <1319825810@qq.com>
2026-05-18 16:54:01 +0800

    update

--- evalution.py
+++ evalution.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     #   （一）此处获得的mAP为验证集的mAP。
     #   （二）此处设置评估参数较为保守，目的是加快评估速度。
     #------------------------------------------------------------------#
-    eval_flag           = False
+    eval_flag           = True
     eval_period         = 5
 
     #------------------------------------------------------------------#
@@ -389,172 +389,25 @@ if __name__ == "__main__":
     #---------------------------#
     #   读取数据集对应的txt
     #---------------------------#
-    with open(os.path.join(VOCdevkit_path, "MIPC_SemanticSegmentation/2007_train.txt"),"r") as f:
-        train_lines = f.readlines()
     with open(os.path.join(VOCdevkit_path, "MIPC_SemanticSegmentation/2007_val.txt"),"r") as f:
         val_lines = f.readlines()
-    num_train   = len(train_lines)
     num_val     = len(val_lines)
 
+
+    #----------------------#
+    #   记录eval的map曲线
+    #----------------------#
     if local_rank == 0:
-        show_config(
-            num_classes = num_classes, phi = phi, model_path = model_path, input_shape = input_shape, \
-            Init_Epoch = Init_Epoch, Freeze_Epoch = Freeze_Epoch, UnFreeze_Epoch = UnFreeze_Epoch, Freeze_batch_size = Freeze_batch_size, Unfreeze_batch_size = Unfreeze_batch_size, Freeze_Train = Freeze_Train, \
-            Init_lr = Init_lr, Min_lr = Min_lr, optimizer_type = optimizer_type, momentum = momentum, lr_decay_type = lr_decay_type, \
-            save_period = save_period, save_dir = save_dir, num_workers = num_workers, num_train = num_train, num_val = num_val
-        )
-        #---------------------------------------------------------#
-        #   总训练世代指的是遍历全部数据的总次数
-        #   总训练步长指的是梯度下降的总次数 
-        #   每个训练世代包含若干训练步长，每个训练步长进行一次梯度下降。
-        #   此处仅建议最低训练世代，上不封顶，计算时只考虑了解冻部分
-        #----------------------------------------------------------#
-        wanted_step = 1.5e4 if optimizer_type == "adamw" else 0.5e4
-        total_step  = num_train // Unfreeze_batch_size * UnFreeze_Epoch
-        if total_step <= wanted_step:
-            if num_train // Unfreeze_batch_size == 0:
-                raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
-            wanted_epoch = wanted_step // (num_train // Unfreeze_batch_size) + 1
-            print("\n\033[1;33;44m[Warning] 使用%s优化器时，建议将训练总步长设置到%d以上。\033[0m"%(optimizer_type, wanted_step))
-            print("\033[1;33;44m[Warning] 本次运行的总训练数据量为%d，Unfreeze_batch_size为%d，共训练%d个Epoch，计算出总训练步长为%d。\033[0m"%(num_train, Unfreeze_batch_size, UnFreeze_Epoch, total_step))
-            print("\033[1;33;44m[Warning] 由于总训练步长为%d，小于建议总步长%d，建议设置总世代为%d。\033[0m"%(total_step, wanted_step, wanted_epoch))
-
-    #------------------------------------------------------#
-    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
-    #   也可以在训练初期防止权值被破坏。
-    #   Init_Epoch为起始世代
-    #   Interval_Epoch为冻结训练的世代
-    #   Epoch总训练世代
-    #   提示OOM或者显存不足请调小Batch_size
-    #------------------------------------------------------#
-    if True:
-        UnFreeze_flag = False
-        #------------------------------------#
-        #   冻结一定部分训练
-        #------------------------------------#
-        if Freeze_Train:
-            for param in model.backbone.parameters():
-                param.requires_grad = False
-
-        #-------------------------------------------------------------------#
-        #   如果不冻结训练的话，直接设置batch_size为Unfreeze_batch_size
-        #-------------------------------------------------------------------#
-        batch_size = Freeze_batch_size if Freeze_Train else Unfreeze_batch_size
-
-        #-------------------------------------------------------------------#
-        #   判断当前batch_size，自适应调整学习率
-        #-------------------------------------------------------------------#
-        nbs             = 16
-        lr_limit_max    = 1e-4 if optimizer_type in ['adam', 'adamw'] else 5e-2
-        lr_limit_min    = 3e-5 if optimizer_type in ['adam', 'adamw'] else 5e-4
-        Init_lr_fit     = min(max(batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
-        Min_lr_fit      = min(max(batch_size / nbs * Min_lr, lr_limit_min * 1e-2), lr_limit_max * 1e-2)
-
-        #---------------------------------------#
-        #   根据optimizer_type选择优化器
-        #---------------------------------------#
-        optimizer = {
-            'adam'  : optim.Adam(model.parameters(), Init_lr_fit, betas = (momentum, 0.999), weight_decay = weight_decay),
-            'adamw' : optim.AdamW(model.parameters(), Init_lr_fit, betas = (momentum, 0.999), weight_decay = weight_decay),
-            'sgd'   : optim.SGD(model.parameters(), Init_lr_fit, momentum = momentum, nesterov=True, weight_decay = weight_decay)
-        }[optimizer_type]
-
-        #---------------------------------------#
-        #   获得学习率下降的公式
-        #---------------------------------------#
-        lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, UnFreeze_Epoch)
-        
-        #---------------------------------------#
-        #   判断每一个世代的长度
-        #---------------------------------------#
-        epoch_step      = num_train // batch_size
-        epoch_step_val  = num_val // batch_size
-        
-        if epoch_step == 0 or epoch_step_val == 0:
-            raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
-        
-        train_dataset   = SegmentationDataset(train_lines, input_shape, num_classes, True, VOCdevkit_path)
-        val_dataset     = SegmentationDataset(val_lines, input_shape, num_classes, False, VOCdevkit_path)
+        eval_callback   = EvalCallback(net=model, input_shape=input_shape, num_classes=num_classes, image_ids=val_lines, dataset_path=VOCdevkit_path,
+                                        log_dir=log_dir, cuda=Cuda, train_name=args.wandb_name, eval_flag=eval_flag, period=eval_period, local_rank=local_rank)
+    else:
+        eval_callback   = None
     
-        if distributed:
-            train_sampler   = torch.utils.data.distributed.DistributedSampler(train_dataset, shuffle=True,)
-            val_sampler     = torch.utils.data.distributed.DistributedSampler(val_dataset, shuffle=False,)
-            batch_size      = batch_size // ngpus_per_node
-            shuffle         = False
-        else:
-            train_sampler   = None
-            val_sampler     = None
-            shuffle         = True
-
-        gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
-                                    drop_last = True, collate_fn = seg_dataset_collate, sampler=train_sampler, 
-                                    worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
-        gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
-                                    drop_last = True, collate_fn = seg_dataset_collate, sampler=val_sampler, 
-                                    worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
-
-        #----------------------#
-        #   记录eval的map曲线
-        #----------------------#
-        if local_rank == 0:
-            eval_callback   = EvalCallback(net=model, input_shape=input_shape, num_classes=num_classes, image_ids=val_lines, dataset_path=VOCdevkit_path,
-                                           log_dir=log_dir, cuda=Cuda, train_name=args.wandb_name, eval_flag=eval_flag, period=eval_period, local_rank=local_rank)
-        else:
-            eval_callback   = None
+    # ---------------------------------------#
+    #   模型性能测试
+    # ---------------------------------------#
+    epoch = 0
+    model_train_eval = model_train.eval()
+    # eval_callback.on_epoch_end(epoch, model_train_eval)
+    eval_callback.on_epoch_end(epoch, model_train_eval)
         
-        #---------------------------------------#
-        #   开始模型训练
-        #---------------------------------------#
-        for epoch in range(Init_Epoch, UnFreeze_Epoch):
-            #---------------------------------------#
-            #   如果模型有冻结学习部分
-            #   则解冻，并设置参数
-            #---------------------------------------#
-            if epoch >= Freeze_Epoch and not UnFreeze_flag and Freeze_Train:
-                batch_size = Unfreeze_batch_size
-
-                #-------------------------------------------------------------------#
-                #   判断当前batch_size，自适应调整学习率
-                #-------------------------------------------------------------------#
-                nbs             = 16
-                lr_limit_max    = 1e-4 if optimizer_type in ['adam', 'adamw'] else 5e-2
-                lr_limit_min    = 3e-5 if optimizer_type in ['adam', 'adamw'] else 5e-4
-                Init_lr_fit     = min(max(batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
-                Min_lr_fit      = min(max(batch_size / nbs * Min_lr, lr_limit_min * 1e-2), lr_limit_max * 1e-2)
-                #---------------------------------------#
-                #   获得学习率下降的公式
-                #---------------------------------------#
-                lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, UnFreeze_Epoch)
-                    
-                for param in model.backbone.parameters():
-                    param.requires_grad = True
-                            
-                epoch_step      = num_train // batch_size
-                epoch_step_val  = num_val // batch_size
-
-                if epoch_step == 0 or epoch_step_val == 0:
-                    raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
-
-                gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
-                                            drop_last = True, collate_fn = seg_dataset_collate, sampler=train_sampler, 
-                                            worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
-                gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
-                                            drop_last = True, collate_fn = seg_dataset_collate, sampler=val_sampler, 
-                                            worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
-
-                UnFreeze_flag   = True
-
-            if distributed:
-                train_sampler.set_epoch(epoch)
-
-            set_optimizer_lr(optimizer, lr_scheduler_func, epoch)
-            
-            fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, epoch, 
-                epoch_step, epoch_step_val, gen, gen_val, UnFreeze_Epoch, Cuda, dice_loss, focal_loss, cls_weights, num_classes, fp16, scaler, save_period, save_dir, local_rank,
-                weight_save_dir=weight_save_dir)
-
-            if distributed:
-                dist.barrier()
-
-        if local_rank == 0:
-            loss_history.writer.close()
